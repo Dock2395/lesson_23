0e804c5deb5027c9069ad0e95abb9faf4bb6dbe4
Dock2395 <43069756+Dock2395@users.noreply.github.com>
2022-12-11 02:52:47 +0300

    new

---
+++ js/js.js
@@ -0,0 +1,15 @@
+//console.log('35' + - "22");
+
+//console.log('35' * "22");
+
+//let varOne = 22;
+//console.log('558' > varOne++);
+
+//let usersCounter = 0;
+//let newUsers = usersCounter++;
+//console.log(newUsers);
+
+//console.log(!false && 11 || 18 && ! '');
+
+let name = 0;
+console.log(name ?? "Без імені");
