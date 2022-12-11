92b2d8a9798663b3f43b42af09971e20050d86a1
Dock2395 <43069756+Dock2395@users.noreply.github.com>
2022-12-12 00:12:14 +0300

    new

--- js/js.js
+++ js/js.js
@@ -11,5 +11,45 @@
 
 //console.log(!false && 11 || 18 && ! '');
 
-let name = 0;
-console.log(name ?? "Без імені");
+//let name = 0;
+//console.log(name ?? "Без імені");
+//================================================
+//Відео 2
+
+//Варіант №3
+//let message = (92 > '11' && 58 < 100) ? 'Правда' : 'Брехня';
+//console.log(message)
+
+//Варіант №4
+//if (0) {
+//	console.log('Брехня');
+//} else if (" ") {
+//	console.log('Правда');
+//}
+
+//================================================
+//Відео 3
+//Завдання №1 - відповідь
+//for (let num = 1; num < 6; num++) {console.log(num);}
+
+//Завдання №2
+//let num = 8;
+//while (num) {
+//	console.log(num);
+//	num--;
+//}
+
+//Завдання №3
+//let num = 0;
+//do {
+//	console.log(`Число: ${num}`);
+//	num++
+//} while (num < 3);
+
+//Завдання №3
+for (let num = 0; num < 2; num++) {
+	for (let size = 0; size < 3; size++) {
+		console.log(size);
+		if (size == 1) break;
+	};
+}
