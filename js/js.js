51853ecf75a663bb6ae12085bceda0e3e728d905
Dock2395 <43069756+Dock2395@users.noreply.github.com>
2022-12-16 21:56:45 +0300

    new

--- js/js.js
+++ js/js.js
@@ -1,16 +1,24 @@
+//================================================
+//Відео 1
+//Варіант №1
 //console.log('35' + - "22");
 
+//Варіант №2
 //console.log('35' * "22");
 
+//Варіант №3
 //let varOne = 22;
 //console.log('558' > varOne++);
 
+//Варіант №4
 //let usersCounter = 0;
 //let newUsers = usersCounter++;
 //console.log(newUsers);
 
+//Варіант №5
 //console.log(!false && 11 || 18 && ! '');
 
+//Варіант №6
 //let name = 0;
 //console.log(name ?? "Без імені");
 //================================================
@@ -47,9 +55,59 @@
 //} while (num < 3);
 
 //Завдання №4
-for (let num = 0; num < 2; num++) {
-	for (let size = 0; size < 3; size++) {
-		console.log(size);
-		if (size == 1) break;
-	};
-}
+//for (let num = 0; num < 2; num++) {
+//	for (let size = 0; size < 3; size++) {
+//		console.log(size);
+//		if (size == 1) break;
+//	};
+//}
+
+//================================================
+//Відео 4
+//Завдання №1
+//let numOne = Math.round(1.005 * 100) / 100;
+//console.log(numOne + Number.EPSILON);
+
+//Завдання №2
+//let value = "135.58px"
+//console.log(parseFloat(value));
+
+//Завдання №3
+//let value = 58 + "Фрілансер";
+//if (isNaN(value)){
+//	console.log('Результат виразу NaN')
+//}
+
+//Завдання №4
+//let maxNumber = Math.max(10, 58, 39, -150, 0);
+//console.log(maxNumber);
+
+//console.log(Math.round(58.858));
+
+//================================================
+//Відео 5
+//Завдання №1
+//let fls = "фрілансер";
+//let text = 'Привіт! Я $(fls)';
+//let text = `Привіт! Я ${fls}`;
+//console.log(text);
+
+//Завдання №2
+//let text = 'фрілансер';
+//console.log(text.slice(5,6));
+
+//Завдання №3
+//let text = 123 + "456";
+//console.log(text);
+
+//Завдання №4
+//let text = 'фрілансер';
+//console.log(text.toUpperCase());
+
+//Завдання №5
+//let text = 'фрілансер';
+//console.log(text.slice(3,6));
+
+//Завдання №6
+//let text = 'фрілансер';
+//console.log(text.slice('лан',3));
